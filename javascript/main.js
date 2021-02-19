3cb107e096f7681b243013904f312cd2a8e1f202
Srdan Cvijetic <scwebdevsc@gmail.com>
2021-02-19 12:49:56 +0100

    Update for Portfolio project 1

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,9 @@ let collapse = document.querySelectorAll("#header .collapse");
 toggle.addEventListener('click', function() {
 collapse.forEach(col=> col.classList.toggle("collapse-toggle"));
 });
+
+/* MASONRY LIBRARY */
+new Masonry("#posts .grid", {
+  itemSelector : '.grid-item',
+  gutter : 20
+});
