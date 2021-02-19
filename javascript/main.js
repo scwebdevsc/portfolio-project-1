dd0a90fc0568cc9708ad6f8ecc907c862fb38638
Srdan Cvijetic <scwebdevsc@gmail.com>
2021-02-19 14:14:26 +0100

    Update for Portfolio project 1

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,31 @@ new Masonry("#posts .grid", {
   itemSelector : '.grid-item',
   gutter : 20
 });
+
+/* SWIPER LIBRARY */
+new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  autoplay: {
+    delay: 3000
+  },
+  // responsive breakpoints
+  breakpoints: {
+    '@0': {
+      slidesPerView: 2
+    },
+    // 888px
+    '@1.00': {
+      slidesPerView: 3
+    },
+    // 1110px
+    '@1.25': {
+      slidesPerView: 4
+    },
+    // 1330px
+    '@1.50': {
+      slidesPerView: 5 
+    }
+  }
+})
